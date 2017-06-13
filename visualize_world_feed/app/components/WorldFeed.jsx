b4e13e56f6edaae0b2e8f7258911a3628b698858
Philip Kurmann <philip.kurmann@peaq.ch>
2017-06-13 09:32:24 +0200

    Worldfeed Visualization Frontend with Node.js

--- visualize_world_feed/app/components/WorldFeed.jsx
+++ visualize_world_feed/app/components/WorldFeed.jsx
@@ -1,9 +1,15 @@
 var React = require("react");
+var ReactHighcharts = require('react-highcharts');
+var ReactDOM = require('react-dom');
+
 const io = require('socket.io-client')  
-const socket = io()  
+const socket = io();
 
 
 module.exports = React.createClass({
+    knownCountries: [],
+    knownLanguages: [],
+
     getInitialState: function(){
         return { searchString: '', data: {} };
     },
@@ -14,21 +20,38 @@ module.exports = React.createClass({
 
     pushData: function(d) {
         var v          = JSON.parse(d);
-        start_ts       = v['start'];
-        end_ts         = v['end'];
+        start_ts       = new Date(v['start']);
+        end_ts         = new Date(v['end']);
         server_country = v['server_country'];
         language       = v['language'];
         count          = v['count'];
 
         data           = this.state.data;
 
-        if (! (start_ts in data))
+        // Remember countries and langagues in a list
+        if (this.knownCountries.indexOf(server_country) < 0)
+          this.knownCountries.push(server_country);
+        if (this.knownLanguages.indexOf(language) < 0)
+          this.knownLanguages.push(language);
+
+        // Initialize a new Timestamp
+        if (! (start_ts in data)) {
             data[start_ts] = {language: {}, country: {}};
+            this.knownCountries.forEach(function(c) { 
+              data[start_ts]['country'][c] = 0;
+            });
+            this.knownLanguages.forEach(function(l) { 
+              data[start_ts]['language'][l] = 0;
+            });
+        }
+
+        // Handle new countries and languages for the current timestamp
         if (! (server_country in data[start_ts]['country']))
             data[start_ts]['country'][server_country] = 0;
         if (! (language in data[start_ts]['language']))
             data[start_ts]['language'][language] = 0;
 
+        // Count!
         data[start_ts]['country'][server_country] += count;
         data[start_ts]['language'][language] += count;
 
@@ -40,26 +63,70 @@ module.exports = React.createClass({
     },
 
     render: function() {
-        var that = this;
-        const config = {
-          /* HighchartsConfig */
+        var that = this; 
+        var language_series = Object.keys(that.knownLanguages).map(function(i) {
+                                lang = that.knownLanguages[i];
+                                // Reformat timeseries for Highcharts
+                                return { name: lang, 
+                                         data: Object.keys(that.state.data).map(function(ts) {
+                                                 return that.state.data[ts]['language'][lang] || 0
+                                               })
+                                       }
+                              });
+        var country_series = Object.keys(that.knownCountries).map(function(i) {
+                                country = that.knownCountries[i];
+                                // Reformat timeseries for Highcharts
+                                return { name: country, 
+                                         data: Object.keys(that.state.data).map(function(ts) {
+                                                 return that.state.data[ts]['country'][country] || 0
+                                               })  
+                                       }
+                              });
+        /* HighchartsConfig */
+        timestamps = Object.keys(that.state.data)
+        var language_config = {
+          title: {text: 'Languages'},
+          plotOptions: {
+              line: {
+                  animation: false
+              }
+          },
+          xAxis: {
+            categories: timestamps,
+            type: 'datetime'
+          },
+          series: language_series
+        };
+        var country_config = {
+          title: {text: 'Countries'},
+          plotOptions: {
+              line: {
+                  animation: false
+              }
+          },
+          xAxis: {
+            categories: timestamps,
+            type: 'datetime'
+          },
+          series: country_series
         };
 
         return <div>
-                  <h1>Hello</h1>
-                  { Object.keys(that.state.data).map(function (key) {
+                  <ReactHighcharts config={language_config} />
+                  <ReactHighcharts config={country_config} />
+                  { Object.keys(that.state.data).map(function (timestamp) {
                       return <div>
-                               <h3>{key}</h3>
+                               <h3>{timestamp}</h3>
                                <h4>Languages</h4>
                                <ul>
-                                 { Object.keys(that.state.data[key]['language']).map(function (key2) {
-                                   return <li>{key2}: {that.state.data[key]['language'][key2]}</li>
+                                 { Object.keys(that.state.data[timestamp]['language']).map(function (language) {
+                                   return <li>{language}: {that.state.data[timestamp]['language'][language]}</li>
                                  }) }
                                </ul> 
                                <h4>Countries</h4>
                                <ul>
-                                 { Object.keys(that.state.data[key]['country']).map(function (key2) {
-                                   return <li>{key2}: {that.state.data[key]['country'][key2]}</li>
+                                 { Object.keys(that.state.data[timestamp]['country']).map(function (country) {
+                                   return <li>{country}: {that.state.data[timestamp]['country'][country]}</li>
                                  }) }
                                </ul> 
                              </div>
